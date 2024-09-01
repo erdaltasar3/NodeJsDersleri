9dfad6c9cde7109ca9b3fa3ec629fa9bcfa25e1d
erdaltasar3 <erdaltasar24@gmail.com>
2024-09-01 21:38:56 +0300

    database connection path1

--- index.js
+++ index.js
@@ -5,12 +5,29 @@ app.set("view engine","ejs");
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
 
+const mysql = require("mysql2");
+
+let connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "12345678",
+    database: "nodedb"
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("mysql baglantisi basarili");
+    }
+})
+
 
 
 const data = [
-    {id:1, name:"iphone 14", price: 30000, isActive: true, imageUrl:"1.jpeg"},
-    {id:2, name:"iphone 15", price: 40000, isActive: false, imageUrl:"2.jpeg"},
-    {id:3, name:"iphone 16", price: 50000, isActive: true, imageUrl:"3.jpeg"},
+    {id:1, name:"iphone 14", price: 30000, isActive: true, imageUrl:"1.jpeg", isHome: true},
+    {id:2, name:"iphone 15", price: 40000, isActive: false, imageUrl:"2.jpeg", isHome:false},
+    {id:3, name:"iphone 16", price: 50000, isActive: true, imageUrl:"3.jpeg", isHome: true},
 ]
 
 // routes
@@ -27,7 +44,9 @@ app.use("/products", function(req,res) {
 });
 
 app.use("/", function(req,res) {
-    res.render("index")
+    res.render("index", {
+        urunler:data
+    });
 });
 
 app.listen(3000, () => {
